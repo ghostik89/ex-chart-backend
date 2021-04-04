c6841a7677a5e3743d6f71c54752fca5b9291cbc
Lady_Proger <facktartar@mail.com>
2021-04-04 22:42:59 +0300

    add cors to app

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require('cors')
 require('dotenv').config()
 const app = express();
 
 //middleware
 app.use(express.json());
+app.use(cors())
 
 app.use('/api/chart', require('./chart.router'))
 
